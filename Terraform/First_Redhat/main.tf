3ed9db0d50a5f01f0875f3cc332d996b5842c666
Sasa Modrakovic <shetkantoor@gmail.com>
2023-02-14 11:58:20 +0100

    Terrafor first server in aws

---
+++ Terraform/First_Redhat/main.tf
@@ -0,0 +1,27 @@
+/*
+provider "aws" {
+  region = var.region
+}
+amazon/RHEL-9.0.0_HVM-20230127-x86_64-24-Hourly2-GP2
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+*/
+
+resource "aws_instance" "redhat8" {
+  ami           = var.redhat_ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+}
---
+++ Terraform/First_Redhat/output.tf
@@ -0,0 +1,8 @@
+/*
+output "instance_ami" {
+  value = aws_instance.redhat.ami
+}
+output "instance_arn" {
+  value = aws_instance.redhat.arn
+}
+*/
