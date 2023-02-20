bd1411348ee3815cc6d514bc059840416ab1ad69
Sasa Modrakovic <shetkantoor@gmail.com>
2023-02-20 15:02:38 +0100

    key naam

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "redhat9_1" {
   ami           = data.aws_ami.rhel_9_1.id
   count         = 2
   instance_type = var.instance_type
-  key_name      = "sasatraining"
+  key_name      = "training_terraform_aws"
 
   tags = {
     Name = "${var.instance_name}_${count.index + 1}"
