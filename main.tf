8af645a483d884e917ae8bd7ee8163a415d6265b
Sasa Modrakovic <shetkantoor@gmail.com>
2023-02-20 14:46:37 +0100

    micro ec2

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "redhat9_1" {
   ami           = data.aws_ami.rhel_9_1.id
   count         = 2
   instance_type = var.instance_type
+  key_name      = "sasatraining"
 
   tags = {
     Name = "${var.instance_name}_${count.index + 1}"
