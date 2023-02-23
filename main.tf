bc3c38022cfeabaef29cc211da4576e55d52332c
Sasa Modrakovic <shetkantoor@gmail.com>
2023-02-23 15:10:57 +0100

    tags weg

--- main.tf
+++ main.tf
@@ -3,17 +3,12 @@ resource "aws_instance" "redhat9_1" {
   count         = 1
   instance_type = var.instance_type
   key_name      = "training_terraform_aws"
-  user_data = <<-EOL
+  user_data     = <<-EOL
   #!/bin/bash
 
   apt update
   subscription-manager register --username ec2-uaer --password *!M@n@g3r!* --auto-attach
   EOL
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
 
   tags = {
     Name = "${var.instance_name}_${count.index + 1}"
