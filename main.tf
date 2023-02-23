81e8905623e1b748afd5408fc35c7dd5bb1a1ff8
Sasa Modrakovic <shetkantoor@gmail.com>
2023-02-23 15:06:01 +0100

    Subsription

--- main.tf
+++ main.tf
@@ -1,8 +1,19 @@
 resource "aws_instance" "redhat9_1" {
   ami           = data.aws_ami.rhel_9_1.id
-  count         = 2
+  count         = 1
   instance_type = var.instance_type
   key_name      = "training_terraform_aws"
+  user_data = <<-EOL
+  #!/bin/bash
+
+  apt update
+  subscription-manager register --username ec2-uaer --password *!M@n@g3r!* --auto-attach
+  EOL
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
 
   tags = {
     Name = "${var.instance_name}_${count.index + 1}"
