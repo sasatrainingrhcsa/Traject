dc04075bfb1d2b2eca588fd36d5fecc064c17901
Sasa Modrakovic <shetkantoor@gmail.com>
2023-02-24 16:58:03 +0100

    isntall ansible

--- main.tf
+++ main.tf
@@ -3,14 +3,25 @@ resource "aws_instance" "redhat9_1" {
   count         = 1
   instance_type = var.instance_type
   key_name      = "training_terraform_aws"
-  user_data     = <<-EOL
-  #!/bin/bash
 
-  apt update
-  subscription-manager register --username sasaguzi --password 12M@n@g3r12 --auto-attach
-  EOL
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y software-properties-common",
+      "sudo apt-add-repository -y ppa:ansible/ansible",
+      "sudo apt-get update",
+      "sudo apt-get install -y ansible",
+    ]
+  }
 
   tags = {
     Name = "${var.instance_name}_${count.index + 1}"
   }
 }
+
+#  user_data     = <<-EOL
+#  #!/bin/bash
+#
+#  apt update
+#  subscription-manager register --username ec2-uaer --password *!M@n@g3r!* --auto-attach
+#  EOL
